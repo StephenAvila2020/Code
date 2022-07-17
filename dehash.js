2c74b762d1fa1169519056e15a49aa739b2f5e15
StephenAvila2020 <stephen.r.avila@gmail.com>
2022-07-16 21:39:38 -0400

    updated url query

--- dehash.js
+++ dehash.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const emails = {
   method: 'GET',
-  url: 'https://api.dehashed.com/search?query=username:"test"&page=2&size=1',
+  url: 'https://api.dehashed.com/search?query=email:',
   headers: {
     'Accept': 'application/json',
     'Authorization': 'Basic ' + btoa('')
